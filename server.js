6fbb1b542a481c9fa2851b9bfe3dc86249984d35
Brixton <auvray.cedric00@gmail.com>
2026-01-10 01:27:10 +0100

    fix: switch to bcryptjs to prevent railway crash

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require('./app');
 
+console.log("🕵️ VALEUR DE process.env.PORT :", process.env.PORT);
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
